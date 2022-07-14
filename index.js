6fb2b1261c638c53fb03a74845d7f7ae1e6c0be7
Shing Fung Lam <shing2888@gmail.com>
2022-07-14 18:20:02 -0500

    update

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/api/users/:_id/exercises', (req,res) => {
             //console.log(logs);
             const filteredLogs = logs
             .filter(log => {
-              const formattedLogDate = (new Date(log.date)).toISOString().split('T')[0]
+              const formattedLogDate = (new Date(log.date)).dateString().split('T')[0]
               //console.log(formattedLogDate)
               return true
             })
